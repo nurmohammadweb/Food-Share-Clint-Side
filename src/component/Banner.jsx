b3a6905e2d6c4a7b259ea74cd573a22dc0f4f5b3
nurmohammadweb <nurmohammad11266@gmail.com>
2025-11-15 00:18:19 +0600

    added Banner desgin with bg img

--- src/component/Banner.jsx
+++ src/component/Banner.jsx
@@ -3,19 +3,16 @@ import { Link } from 'react-router';
 
 const Banner = () => {
   return (
-    <div className="flex flex-col items-center justify-center text-center py-10 px-6  from-orange-50 to-white">
+    <div className="flex  justify-between https://i.ibb.co.com/vCBs79gw/f3.jpg text-center  from-orange-50 to-white">
     
       <div className="max-w-4xl"
-       style={{
-    backgroundImage:
-      "url(https://i.ibb.co.com/vCBs79gw/f3.jpg)",
-  }}
+      
       >
           <h1 className="text-3xl sm:text-4xl md:text-5xl font-bold text-red-700 mb-5">
           Food Sharing
         </h1>
         
-        <p className="text-white text-sm sm:text-base md:text-lg mb-6 leading-relaxed mt-10">
+        <p className="text-black text-sm sm:text-base md:text-lg mb-6 leading-relaxed mt-10">
           PlateShare — A vibrant food sharing community where people connect to reduce food waste and help others. 
           Share surplus meals, discover local food donations, and make every plate count. 
           Together, we create kindness through food and community.
@@ -25,6 +22,9 @@ const Banner = () => {
             View All Foods
           </button>
         </Link>
+      </div>
+      <div>
+        <img className='' src="https://i.ibb.co.com/vCBs79gw/f3.jpg" alt="" />
       </div>
     </div>
   );
